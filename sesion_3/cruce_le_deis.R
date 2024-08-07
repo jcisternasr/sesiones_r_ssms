f678759e9df17829dfec6ae4ad9df764a0afaf71
Ricardo Ahumada O. <r_ahumada@uchile.cl>
2024-08-07 10:22:10 -0400

    sesion3

--- sesion_3/cruce_le_deis.R
+++ sesion_3/cruce_le_deis.R
@@ -1,5 +1,3 @@
-#install.packages('pacman')
-
 # Importar librerías 
 pacman::p_load(
   readxl, #importar archivos excel
@@ -11,31 +9,20 @@ pacman::p_load(
   summarytools,
   rio
 )
-# Definir la ruta del archivo
-ruta <- here('sesion_3',"data","data_le_anonima.xlsx")
-# Importar el archivo
-data <- import(ruta) 
-# Revisar columnas
-names(data)
-# Revisar los datos
-dfSummary(data)
 
-# Mismo formato de las columnas
-data <- data %>%
-  clean_names()
-
-# importar base deis
-ruta <- here('sesion_3',"data","deis2024.xlsx")
-
-deis <- import(ruta) %>%
+# Importar datos DEIS
+ruta2 <- here('sesion_3',"data","deis2024.xlsx")
+deis <- import(ruta2) %>%
   clean_names() %>%
   select(codigo_vigente, nombre_oficial)  %>% 
   mutate(
     codigo_vigente = as.character(codigo_vigente)
   )
 
-# cruzar los datos de LE y DEIS 
-data <- data %>%
+#Importar datos de LE 
+ruta <- here('sesion_3',"data","data_le_anonima.xlsx")
+data <- import(ruta) %>%
+  clean_names() %>% 
   left_join(deis, by=c('estab_orig'='codigo_vigente'))
   
 
