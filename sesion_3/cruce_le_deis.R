3bb5490c0449213ff3c8d44276d79c061f5712c5
Ricardo Ahumada O. <r_ahumada@uchile.cl>
2024-08-13 20:45:42 -0400

    Update cruce_le_deis.R

--- sesion_3/cruce_le_deis.R
+++ sesion_3/cruce_le_deis.R
@@ -10,7 +10,7 @@ pacman::p_load(
   rio
 )
 
-# Importar datos DEIS
+# Importar datos DEIS establecimiento de destino
 ruta2 <- here('sesion_3',"data","deis2024.xlsx")
 deis <- import(ruta2) %>%
   clean_names() %>%
@@ -19,11 +19,32 @@ deis <- import(ruta2) %>%
     codigo_vigente = as.character(codigo_vigente)
   )
 
+# Importar datos DEIS para establecimiento de origen
+
+deis_origen <- import(ruta2) %>%
+  clean_names() %>%
+  select(codigo_vigente, nombre_oficial, nombre_comuna)  %>% 
+  mutate(
+    codigo_vigente = as.character(codigo_vigente)
+  )
+
+
+# Importar datos especialidades
+ruta3 <- here('sesion_3',"data","especialidades.xlsx")
+esp <- import(ruta3) %>%
+  clean_names() %>%
+  select(codigosigte, desc820)
+
 #Importar datos de LE 
 ruta <- here('sesion_3',"data","data_le_anonima.xlsx")
-data <- import(ruta) %>%
+
+data <- import(ruta)
+data2 <- data %>%
   clean_names() %>% 
-  left_join(deis, by=c('estab_orig'='codigo_vigente'))
+  left_join(deis, by=c('estab_dest'='codigo_vigente')) %>%
+  left_join(esp, by=c('presta_min'='codigosigte')) %>%
+  left_join(deis_origen, by=c('estab_orig'='codigo_vigente')) %>% 
+  filter(nombre_comuna == 'Calera de Tango')
   
 
 
